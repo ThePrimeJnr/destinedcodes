e794fe110ccf3aaf35893f74ec6ce3c244d0a84c
Destiny Saturday <yiradesat@gmail.com>
2024-06-21 16:12:58 +0100

    🏗️ build(redirect): remove redirrect from next config

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const withMDX = require('@next/mdx')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'header', key: 'host', value: 'destinedcodes.com' }],
-        destination: 'https://www.destinedcodes.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = withMDX(nextConfig);
