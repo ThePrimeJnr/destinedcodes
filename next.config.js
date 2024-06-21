83b11f9e7ed560286dd19216cf097382f524d0fa
Destiny Saturday <yiradesat@gmail.com>
2024-06-21 15:03:46 +0100

    🐛 fix(redirect): fix issues redirecting non www to www

--- next.config.js
+++ next.config.js
@@ -2,9 +2,17 @@ const withMDX = require('@next/mdx')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'header', key: 'host', value: 'destinedcodes.com' }],
+        destination: 'https://www.destinedcodes.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withMDX(nextConfig);
